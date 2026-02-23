2a1dc68f4d53fee0b397785af5db0cc35473cf81
Mohammed Sultan Qureshi <G00470222@atu.ie>
2026-02-23 17:00:13 +0000

    more debugging the login function seems to not work

--- Website/Login.js
+++ Website/Login.js
@@ -1,28 +1,31 @@
 const login = document.getElementById("login");
 
-login.addEventListener("click",async function(e){
-   const API = "https://chatwebsite-fws0.onrender.com/";
+login.addEventListener("submit", async function(e) {
+
     e.preventDefault();
 
-    const email=document.getElementById("email").value;
+    const API = "https://chatwebsite-fws0.onrender.com/";
 
-    const password=document.getElementById("password").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-    const response = await fetch(API+"login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json"},
-    body: JSON.stringify({ email, password })
-  });
+    const response = await fetch(API + "login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password })
+    });
 
-  const data = await response.json();
-  document.getElementById('message').innerText = data.message;
+    const data = await response.json();
 
-  localStorage.setItem("UserData",JSON.stringify(data.user));
+    document.getElementById('message').innerText = data.message;
 
-  if (data.message === "Login successful!") {
-  window.location.href = "Home.html";
-}
+    if (data.user) {
+        localStorage.setItem("UserData", JSON.stringify(data.user));
+    }
 
+    if (data.message === "Login successful!") {
+        window.location.href = "Home.html";
+    }
 });
 
   
