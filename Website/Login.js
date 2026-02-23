e51b44fdc58b34b3f1d6ea1cd68299591afcd667
Mohammed Sultan Qureshi <G00470222@atu.ie>
2026-02-23 16:36:08 +0000

    changed the api link the the online link where the server is being hosted

--- Website/Login.js
+++ Website/Login.js
@@ -1,7 +1,7 @@
 const login = document.getElementById("login");
 
 login.addEventListener("click",async function(e){
-   const API = "http://localhost:3000";
+   const API = "https://chatwebsite-fws0.onrender.com/";
     e.preventDefault();
 
     const email=document.getElementById("email").value;
